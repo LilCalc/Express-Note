ddc46be965034ce4208b87d51f567704f17c4a5d
LilCalc <noahsavinois@gmail.com>
2022-06-05 20:17:11 -0500

    Made it better

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-// create a GET route to render the notes.html file "/notes"
-// create a GET route to render index.html "*"
-// create a GET route to read the json file - "/api/notes"
-//TODO: create a GET route to read the json file - "/api/notes"
-//TODO: create a POST route to create a new note and add it to the json file
-//TODO: create a DELETE route containing the id of a note to delete
-
-
 //require dependencies
 const express = require('express');
 
@@ -19,9 +11,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-const apiRoutes = require("./routes/apiRoutes");
+const apiRoutes = require("../miniature-eureka/Routes/apiRoutes");
 app.use(apiRoutes);
-const htmlRoutes = require("./routes/htmlRoutes");
+const htmlRoutes = require("../miniature-eureka/Routes/htmlRoutes");
 app.use(htmlRoutes);
 
 //server listener
