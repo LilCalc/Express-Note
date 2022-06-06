15d694a60b493375a28523369acbf9a8e7327ed2
LilCalc <noahsavinois@gmail.com>
2022-06-05 20:19:02 -0500

    Port Variable

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 //require dependencies
 const express = require('express');
-
-//create express app
-const app = express();
-
 //create PORT variable
 const PORT = process.env.PORT || 3000;
+//create express app
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
